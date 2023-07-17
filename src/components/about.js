c2007aa5ee16de11ce60b73c2e3db16ce68ae978
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-07-17 15:23:31 -0500

    added screen shots of portfolio pages. footer still needs work with positioning.

--- src/components/about.js
+++ src/components/about.js
@@ -4,11 +4,14 @@ function About() {
 
 
     return (
-        <p>
+        <div>
+            
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla at volutpat diam ut venenatis. Mauris pellentesque pulvinar pellentesque habitant morbi tristique. Suspendisse sed nisi lacus sed viverra tellus in hac. Eu nisl nunc mi ipsum faucibus vitae aliquet nec. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Pharetra vel turpis nunc eget lorem dolor. Odio ut enim blandit volutpat maecenas. Amet purus gravida quis blandit turpis cursus in. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Urna duis convallis convallis tellus id interdum. Diam sit amet nisl suscipit adipiscing bibendum est.
 
-            Nisi est sit amet facilisis magna etiam. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo. Orci porta non pulvinar neque laoreet. Urna nec tincidunt praesent semper. Augue eget arcu dictum varius. Enim lobortis scelerisque fermentum dui faucibus in ornare. Massa placerat duis ultricies lacus. Sed odio morbi quis commodo odio. Bibendum at varius vel pharetra vel turpis nunc eget lorem. Risus sed vulputate odio ut. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Tempor orci eu lobortis elementum nibh tellus molestie nunc.
-        </p>
+Nisi est sit amet facilisis magna etiam. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo. Orci porta non pulvinar neque laoreet. Urna nec tincidunt praesent semper. Augue eget arcu dictum varius. Enim lobortis scelerisque fermentum dui faucibus in ornare. Massa placerat duis ultricies lacus. Sed odio morbi quis commodo odio. Bibendum at varius vel pharetra vel turpis nunc eget lorem. Risus sed vulputate odio ut. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Tempor orci eu lobortis elementum nibh tellus molestie nunc.
+
+
+       </div>
     )
 }
 
--- src/components/project.js
+++ src/components/project.js
@@ -4,7 +4,81 @@ function Project() {
 
 
     return (
-        <h1> Projects </h1>
+        <div class="container">
+            <div class="row" >
+                <div class="card col-4">
+                    <a href="https://adam-brannon09.github.io/password_generator/" ><img src="./assets/passwordGif.gif" class="card-img-top" alt=""></img></a>
+                        <div class="card-body">
+                            <h5 class="card-title">Password Generator</h5>
+                            <p class="card-text">Easily Generate Random Passwords</p>
+                            {/* <a href="https://adam-brannon09.github.io/password_generator/" class="btn btn-primary">Password Generator</a> */}
+                        </div>
+                </div>
+
+                <div class="card col-4">
+                    <img src="./assets/workDayGif.gif" class="card-img-top" alt=""></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Work Day Scheduler</h5>
+                            <p class="card-text">A simple daily planner.</p>
+                            <a href="https://adam-brannon09.github.io/work_day_scheduler/" class="btn btn-primary">Work Day Planner</a>
+                        </div>
+                </div>
+
+                <div class="card col-4">
+                    <img src="./assets/codeQuizGif.gif" class="card-img-top" alt=""></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Coding Quiz</h5>
+                            <p class="card-text">A timed quiz to test your coding knowledge</p>
+                            <a href="https://adam-brannon09.github.io/coding_quiz/" class="btn btn-primary">Coding Quiz</a>
+                        </div>
+                </div>
+
+                <div class="card col-4">
+                    <img src="./assets/weatherGif.gif" class="card-img-top" alt=""></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Weather Dashboard</h5>
+                            <p class="card-text">Get any weather info</p>
+                            <a href="https://adam-brannon09.github.io/weather_forecast_app/" class="btn btn-primary">Weather Dashboard</a>
+                        </div>
+                </div>
+
+                <div class="card col-4">
+                    <img src="./assets/moodyGif.gif" class="card-img-top" alt=""></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Moody</h5>
+                            <p class="card-text">Your 5 Minute Journal</p>
+                            <a href="https://tranquil-wildwood-45278.herokuapp.com/login" class="btn btn-primary">Moody</a>
+                        </div>
+                </div>
+
+                <div class="card col-4">
+                    <img src="./assets/noteTakerGif.gif" class="card-img-top" alt=""></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Note Taker</h5>
+                            <p class="card-text">An easy to use note taking app</p>
+                            <a href="https://pure-plateau-06759-17795068f1d2.herokuapp.com/" class="btn btn-primary">Note Taker</a>
+                        </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+
+            {/* End */}
+        </div>
+
+
     )
 }
 
