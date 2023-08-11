5258233e6e4316b5cf9ddb5d0c061e72707a631d
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-08-11 14:20:10 -0500

    added final group project

--- src/components/project.js
+++ src/components/project.js
@@ -9,16 +9,16 @@ function Project() {
         <div className="container tiles portfolio-container ">
 
             <div className="row card-deck">
-               <code><h2 className="depAppHead">{'{Deployed Applications}'}</h2></code>
-                
+                <code><h2 className="depAppHead">{'{Deployed Applications}'}</h2></code>
+
                 {/* JATE */}
                 <div className="card col-4 bg-dark text-light rounded p-2 border border-white">
-                    <a ><img className="card-img-top" src="./assets/jate-demo.gif" alt="Card image cap"></img></a>
+                    <a ><img className="card-img-top" src="./assets/jetsetgo.gif" alt="Card image cap"></img></a>
                     <div className="card-body">
-                        <h5 className="card-title">J.A.T.E Just Another Text Editor</h5>
+                        <h5 className="card-title">Jet Set Go</h5>
                         <br></br>
-                        <button className="btn btn-lg btn-outline-light bg-dark text-light p-2 mx-2"><a className="text-light project-link" href="https://rocky-lake-08346-97f05045468c.herokuapp.com/" >Deployed App</a></button>
-                        <button className="btn btn-lg btn-outline-light bg-dark p-2 mx-2 "><a className="text-light project-link" href="https://github.com/adam-brannon09/PWA_Text_Editor"><i className="devicon-github-original"></i>To The Repo</a></button>
+                        <button className="btn btn-lg btn-outline-light bg-dark text-light p-2 mx-2"><a className="text-light project-link" href="https://salty-tor-71177-880297e382ef.herokuapp.com/" >Deployed App</a></button>
+                        <button className="btn btn-lg btn-outline-light bg-dark p-2 mx-2 "><a className="text-light project-link" href="https://github.com/jhdavey/jsg"><i className="devicon-github-original"></i>To The Repo</a></button>
                     </div>
                 </div>
                 {/* Workday Scheduler */}
@@ -33,7 +33,7 @@ function Project() {
                 </div>
                 {/* Google Books */}
                 <div className="card col-4 bg-dark text-light rounded p-2 border border-white">
-                   <a ><img className="card-img-top" src="./assets/google_books.gif" alt="Card image cap"></img></a>
+                    <a ><img className="card-img-top" src="./assets/google_books.gif" alt="Card image cap"></img></a>
                     <div className="card-body">
                         <h5 className="card-title">Google Books</h5>
                         <br></br>
@@ -47,7 +47,7 @@ function Project() {
 
             <div className="row card-deck justify-content-between">
                 {/* Weather Forecast */}
-            <div className="card col-4 bg-dark text-light rounded p-2 border border-white">
+                <div className="card col-4 bg-dark text-light rounded p-2 border border-white">
                     <a ><img className="card-img-top" src="./assets/weatherGif.gif" alt="Card image cap"></img></a>
                     <div className="card-body">
                         <h5 className="card-title">Weather Forecast App</h5>
@@ -58,7 +58,7 @@ function Project() {
                 </div>
                 {/* Moody */}
                 <div className="card col-4 bg-dark text-light rounded p-2 border border-white">
-                   <a ><img className="card-img-top" src="./assets/moodyGif.gif" alt="Card image cap"></img></a> 
+                    <a ><img className="card-img-top" src="./assets/moodyGif.gif" alt="Card image cap"></img></a>
                     <div className="card-body">
                         <h5 className="card-title">Moody The 5 Minute Journal</h5>
                         <br></br>
@@ -68,7 +68,7 @@ function Project() {
                 </div>
                 {/* Note Taker */}
                 <div className="card col-4 bg-dark text-light rounded p-2 border border-white">
-                <img className="card-img-top" src="./assets/noteTakerGif.gif" alt="Card image cap"></img>
+                    <img className="card-img-top" src="./assets/noteTakerGif.gif" alt="Card image cap"></img>
                     <div className="card-body">
                         <h5 className="card-title">Note Taker</h5>
                         <br></br>
@@ -76,7 +76,7 @@ function Project() {
                         <button className="btn btn-lg btn-outline-light bg-dark p-2 mx-2 "><a className="text-light project-link" href="https://github.com/adam-brannon09/note_taker"><i className="devicon-github-original"></i>To The Repo</a></button>
                     </div>
                 </div>
-            
+
             </div>
 
 
